1604a48f6b35b3b7d124ecf22515b6d33d9b03e6
Jubayer Ahmed <67945738+Jubayer05@users.noreply.github.com>
2022-02-28 06:19:02 +0600

    Conditions

--- conditionals.js
+++ conditionals.js
@@ -2,7 +2,7 @@ var itIsRaining = false;
 var haveUmbralla = true;
 
 if (itIsRaining == true && haveUmbralla == false) {
-    console.log("I will not go out, I will stay home and stay codding untill the rain stop");
+    console.log("I will not go out and keep ut, I will stay home and stay codding untill the rain stop");
 }
 
 else if (itIsRaining == false && haveUmbralla !== true) {
